ef021728016263dbfc93eaacee3070f7915eaed0
ledomone <ledomone@users.noreply.github.com>
2016-02-20 19:11:49 +0100

    Homework 3 - Option 2 - refactor (validation to model, action to route, remove contact controller)

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  isEmailValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  isMessageValid: Ember.computed.gte('message.length', 1),
+  allValid: Ember.computed.and('isEmailValid', 'isMessageValid'),
+  isNotValid: Ember.computed.not('allValid')
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('contact');
+  },
+
   actions: {
+    sendMessage(newContact) {
+      newContact.save().then(() => this.controller.set('responseMessage', true));
+    },
 
     willTransition() {
       let model = this.controller.get('model');
@@ -9,6 +17,8 @@ export default Ember.Route.extend({
       if (model.get('isNew')) {
         model.destroyRecord();
       }
+
+      this.controller.set('responseMessage', false);
     }
   }
 });
