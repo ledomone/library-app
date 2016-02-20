31b8da535ba76c9940a1ba6d697b6e025168ae60
ledomone <ledomone@users.noreply.github.com>
2016-02-20 17:37:59 +0100

    Homework 3a - model for contact

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  emailAddress: '',
-  message: '',
-  isEmailValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  // isMessageValid: Ember.computed('message', function() {
-  //   return this.get('message').length >= 1;
-  // }),
-  isMessageValid: Ember.computed.gte('message.length', 1),
+  isEmailValid: Ember.computed.match('model.email', /^.+@.+\..+$/),
+  isMessageValid: Ember.computed.gte('model.message.length', 1),
   allValid: Ember.computed.and('isEmailValid', 'isMessageValid'),
-  isDisabled:  Ember.computed.not('allValid'),
+  isDisabled: Ember.computed.not('allValid'),
+
+  model: Ember.computed(function() {
+    return this.store.createRecord('contact');
+  }),
   actions: {
     sendMessage() {
-      alert(`email: ${this.get('emailAddress')} \nmessage: ${this.get('message')}`);
-      this.set('responseMessage', 'We got your message and get in touch soon.');
-      this.set('emailAddress', '');
-      this.set('message', '');
+      this.get('model').save().then((response) => {
+        alert(`email: ${this.get('emailAddress')} \nmessage: ${this.get('message')}`);
+        this.set('responseMessage', `We got your message (id: ${response.id})and get in touch soon.`);
+        this.set('model', this.store.createRecord('contact'));
+      });
     }
   },
 });
