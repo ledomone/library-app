9042f0408e07632be398ff28436326d56d32f62f
ledomone <ledomone@users.noreply.github.com>
2016-02-24 23:35:26 +0100

    Homework 4b - refactor app/controllers/index.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,24 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  emailAddress: '',
-  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isValid: Ember.computed.match('model.email', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
-  actualEmailAddress: Ember.computed('emailAddress', function() {
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  actualEmailAddress: Ember.computed('model.email', function() {
+    console.log('actualEmailAddress function is called: ', this.get('model.email'));
   }),
-  emailAddressChanged: Ember.observer('emailAddress', function() {
-    console.log('observer is called', this.get('emailAddress'));
+  emailAddressChanged: Ember.observer('model.email', function() {
+    console.log('observer is called', this.get('model.email'));
   }),
-  actions: {
-    saveInvitation() {
-      const email = this.get('emailAddress');
-
-      const newInvitation = this.store.createRecord('invitation', {email: email});
-      newInvitation.save().then((response) => {
-        this.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
-        this.set('emailAddress', '');
-      });
-    }
-  },
 });
---
+++ app/routes/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('invitation');
+  },
+
+  actions: {
+    saveInvitation(invitation) {
+      invitation.save().then((response) => {
+        this.controller.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
+        this.controller.set('model', this.store.createRecord('invitation'));
+      });
+    }
+  }
+
+});
