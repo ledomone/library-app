133a7b07ca05dddde6a8d1381aa08b62ed4a59e0
ledomone <ledomone@users.noreply.github.com>
2016-02-25 22:51:37 +0100

    Add to invitation model default creation date

--- app/models/invitation.js
+++ app/models/invitation.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  email: DS.attr('string')
+  email: DS.attr('string'),
+  createdAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  })
 });
