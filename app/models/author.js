2e15c50ce14420d3d74c38be19540ede1641d443
ledomone <ledomone@users.noreply.github.com>
2016-02-28 00:45:51 +0100

    Install faker.js for dummy data

--- app/models/author.js
+++ app/models/author.js
@@ -1,6 +1,12 @@
 import DS from 'ember-data';
+import Faker from 'faker';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  books: DS.hasMany('book')
+  books: DS.hasMany('book', {inverse: 'author'}),
+
+  randomize() {
+    this.set('name', Faker.name.findName());
+    return this;
+  }
 });
